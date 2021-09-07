0de6494927efe6c31ed3459ce03c01e407ea3928
kwright-ciat <kwright@ciat.edu>
2021-09-07 15:01:04 -0700

    two changed files

--- auto-build.sh
+++ auto-build.sh
@@ -1,37 +1,51 @@
-if  lsmod | grep -q "88x2bu" 
+#!/usr/bin/env bash
+# autobuild.sh
+# Build and install the 88x2bu module and load it if necessary
+
+if  lsmod | grep -q "88x2bu" # check if module 88x2bu is loaded
 then
-	logger -t WIFI "Module 88x2bu already loaded successfully"
+	logger -t 88x2BU "Module 88x2bu already loaded successfully"
 	exit 0
+else
+	logger -t 88x2BU "Starting build of 88x2bu module"
 fi
-if [ -d /usr/local/src/RTL88x2BU-Linux-Driver/ ]
+
+if [ -d /usr/local/src/RTL88x2BU-Linux-Driver/ ] # check for module driver directory
 then 
 	cd /usr/local/src/RTL88x2BU-Linux-Driver/
 else
-	logger -t WIFI "Driver directory /usr/local/src/RTL88x2BU-Linux-Driver/ not found"
+	logger -t 88x2BU "Driver directory /usr/local/src/RTL88x2BU-Linux-Driver/ not found"
 	exit 1
 fi
 
-if ! make clean 
+if ! make clean # Exit if unable to clean driver directory
 then
-	logger -t WIFI "Unable to clean driver directory"
+	logger -t 88x2BU "Unable to clean driver directory"
 	exit 2
+else
+	logger -t 88x2BU "Successfully cleaned driver directory"
 fi
-if ! make -j 4 
+
+if ! make -j 4 # Compile the module
 then
-	logger -t WIFI "Unable to make driver"
+	logger -t 88x2BU "Unable to compile 88x2bu module"
 	exit 3
+else
+	logger -t 88x2BU "Successfully compiled 88x2bu module"
 fi
 
-if ! make install 
+if ! make install # Install the module
 then
-	logger -t WIFI "Unable to install driver"
+	logger -t 88x2BU "Unable to install 88x2bu module"
 	exit 4
+else 
+	logger -t 88x2BU "Successfully installed 88x2bu module"
 fi
 
-if ! modprobe 88x2bu 
+if ! modprobe 88x2bu # Load the module
 then
-	logger -t WIFI "Module driver failed to load"
+	logger -t 88x2BU "Module 88x2bu failed to load"
 	exit 5
 else
-	logger -t WIFI "Module 88x2bu successfully built and loaded"
+	logger -t 88x2BU "Module 88x2bu successfully loaded"
 fi
